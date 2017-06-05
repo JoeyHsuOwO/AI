8924d1931152cb098f9ec85ef4fc182ebaa40ab6
Joey <gji4gji40318@gmail.com>
2017-06-05 22:07:55 +0800

    WeightCount

--- AIWeightCount/main.cpp
+++ AIWeightCount/main.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 
 struct Weight {
-	int W[3];	/*存取權重值*/
+	int W[3];	/*Weight to save*/
 	char sign;
 };
 
 
 int main()
 {
-	int s[3];/*初始權重*/
-	int sum = 0;/*相加後的結果*/
-	int count = 0;/*正確的次數*/
+	int s[3];/*Initial Wight Array*/
+	int sum = 0;/*sum*/
+	int count = 0;/*correct count*/
 	int cycle = 1;
-	struct Weight I[4];/*權重的數量*/
+	struct Weight I[4];/*count of weight*/
 
-	cout << "初始權重:" << endl;
+	cout << "Initial Wight:" << endl;
 	for (int i = 0; i < 3; i++)
 	{
 		cin >> s[i];
 	}
-	cout <<endl<< "已知權重: ";
-	for (int i = 0; i < 4; i++)/*輸入權重的值*/
+	cout <<endl<< "Wight You Known: ";
+	for (int i = 0; i < 4; i++)/*Input the wieght*/
 	{
 		cout << endl << "I" << i + 1 << ": "<<endl;
 		for (int j = 0; j < 3; j++)
@@ -32,7 +32,7 @@ int main()
 	}
 	cout << "        Chosen Instance  Current Weight Vector" << endl;
 	cout << "Cycle 0       (" << s[0] << "," << s[1] << "," << s[2] << ")" << endl;
-	while (count != 4)/*正確次數為達到4次就繼續迴圈*/
+	while (count != 4)/*if correct count is 4 then circle*/
 	{
 		count = 0;
 		for (int i = 0; i < 4; i++) 
@@ -42,14 +42,14 @@ int main()
 			{
 				sum += s[j] * I[i].W[j];
 			}
-			if (sum <= 0 && I[i].sign == '+')/*如果符號是正的,結果小於等於0*/
+			if (sum <= 0 && I[i].sign == '+')/*if the sign is positive, result <= 0*/
 			{
 				for (int j = 0; j < 3; j++)
 				{
 					s[j] += I[i].W[j];
 				}
 			}
-			else if (sum >= 0 && I[i].sign == '-')/*如果符號是負的,結果大於等於0*/
+			else if (sum >= 0 && I[i].sign == '-')/*if the sign is negative,result >= 0*/
 			{
 				for (int j = 0; j < 3; j++)
 				{
@@ -58,9 +58,9 @@ int main()
 			}
 			else
 			{
-				count++;/*正確次數+1*/
+				count++;/*correct count + 1*/
 			}
-			if (count != 4)/*如果不等於4的話,會一直迴圈遞增cycle*/
+			if (count != 4)/*if count != 4,keep circle*/
 			{
 				cout << "Cycle " << cycle << "  (" << I[i].W[0] << "," << I[i].W[1] << "," << I[i].W[2] << "," << I[i].sign << ")";
 				cout << "     (" << s[0] << "," << s[1] << "," << s[2] << ")" << endl;
@@ -69,7 +69,7 @@ int main()
 			}
 		}
 	}
-	cout << "答案為:" << "(" << s[0] << "," << s[1] << "," << s[2] << ")" << endl;
+	cout << "Answer:" << "(" << s[0] << "," << s[1] << "," << s[2] << ")" << endl;
 	cout << endl;
 	system("pause");		
 }
